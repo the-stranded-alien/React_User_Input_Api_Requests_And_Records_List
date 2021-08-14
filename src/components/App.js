861036bf55ad56f92e554bc89e78f5b59f51b468
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2021-08-14 16:16:17 +0530

    Handling Form Submit, Solving Context Issues and Communicate Child to Parent

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: '20px'}} >
-            <SearchBar />
-        </div>
-    );
-};
+// const App = () => {
+//     return (
+//         <div className="ui container" style={{ marginTop: '20px'}} >
+//             <SearchBar />
+//         </div>
+//     );
+// };
+
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '20px'}} >
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,12 +4,33 @@ class SearchBar extends React.Component {
     // onInputChange(event) { console.log(event.target.value); }
     // onInputClick() { console.log("Input Was Clicked"); }
 
+    // 1st Way
+    // constructor(props) {
+    //     super(props)
+    //     this.state = this.state.bind(this)
+    // }
+
     state = { term: ''};
+
+    // onFormSubmit(event) {
+        // event.preventDefault();
+        // console.log(this.state.term); // Won't Work Here as 'this' is Undefined
+    //}
+
+    // 2nd Way
+    onFormSubmit = event => {
+        event.preventDefault();
+        // This would work
+        // console.log(this.state.term);
+        this.props.onSubmit(this.state.term);
+    }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                {/* 3rd Way */}
+                {/* <form onSubmit={(event) => this.onFormSubmit(event)} className="ui form"> */}
                     <div className="field">
                     <label> Image Search : </label>
                     {/* <input 
